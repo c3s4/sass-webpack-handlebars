0e5763c6a325cae0014a7da3cb1d09478fce3d94
Davide Cesaroni <davide.cesaroni@gmail.com>
2017-01-21 19:17:27 +0100

    Create webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractCSS = new ExtractTextPlugin('[name].bundle.css');
+
+
+module.exports = {
+  entry: ['./app/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/app'
+  },
+  module: {
+    rules: [
+      { 
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      },
+
+      {
+        // Only run `.js` files through Babel
+        test: /\.js?$/,
+        loader: "babel-loader",
+
+        // Skip any files outside of your project's `app` directory
+        include: [
+          path.resolve(__dirname, "app"),
+        ]
+      },
+
+      {
+        test: /\.(css|scss)$/i,
+        exclude: /node_modules/,
+        loader: extractCSS.extract({
+          fallbackLoader: "style-loader",
+          loader: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractCSS
+  ]
+};
